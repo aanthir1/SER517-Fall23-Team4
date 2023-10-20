c65891712e58a0c0f55666b5d23f6fdfb3dec6c5
sairachana97 <spaladu9@asu.edu>
2023-10-20 15:39:41 -0700

    values displaying for a specific column value

--- gui/groupLabelling.py
+++ gui/groupLabelling.py
@@ -16,6 +16,24 @@ class Impl_GroupLabelling_Window(Ui_Dialog, QtWidgets.QMainWindow):
             df = pd.read_csv(dataset_path)  # Use read_excel for Excel files
             column_names = df.columns
             self.comboBox.addItems(column_names)  # Add column names directly to the comboBox
+            
+
+            self.comboBox.currentIndexChanged.connect(self.updateDropdownItems)  # Connect the event handler
         except Exception as e:
             print("Error:", e)
+    
+    def updateDropdownItems(self, index):
+        """Update the dropdown with unique values from the selected column"""
+        selected_column = self.comboBox.currentText()  # Get the selected column name
+        if selected_column:
+            try:
+                df = pd.read_csv(self.path)
+                unique_values = df[selected_column].unique()
+                self.comboBox_2.clear()  # Clear the existing items
+                self.comboBox_2.addItems([str(val) for val in unique_values])
+
+                
+
+            except Exception as e:
+                print("Error:", e)
 
