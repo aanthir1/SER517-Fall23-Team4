d50d7096f8e261cedc9185541c92447d74351455
Aditya S <adityasai.nune@missiong3.com>
2023-11-23 22:52:42 -0700

    Fixed issues related to risk widget

--- gui/risk.py
+++ gui/risk.py
@@ -13,6 +13,7 @@ import json
 from dataset_column import DatasetColumn
 import math
 from PyQt5.QtCore import pyqtSignal
+from pathlib import Path
 
 class Impl_RiskWindow(Ui_RiskWindow, QtWidgets.QMainWindow):
     """Creates risk assessment window"""
@@ -24,9 +25,17 @@ class Impl_RiskWindow(Ui_RiskWindow, QtWidgets.QMainWindow):
         super(Ui_RiskWindow, self).__init__()
         self.setupUi(self)
         self.home_button.triggered.connect(self.home_button_clicked)
-        self.go_back_button.triggered.connect(self.go_back_button_clicked)
+        self.go_back_button.triggered.connect(self.home_button_clicked)
+        current_dir = os.path.abspath(os.path.dirname(__file__))
+        r_path= os.path.abspath('toolfiles/ccode dx labeled.csv')  # Change this to the path of your file
 
-        file_path = r'gui/toolfiles/ccode dx labeled.csv'
+        # Get the relative path of the file from the current directory
+        file_path = os.path.relpath(r_path, current_dir)
+
+        #file_name = "ccode dx labeled.csv"
+        #script_dir = os.path.dirname(os.path.abspath(__file__))
+        #file_path = os.path.join(script_dir, '../toolfiles/ccode dx labeled.csv')
+        #file_path = r'gui/toolfiles/ccode dx labeled.csv'
         self.datasetDF = pd.read_csv(file_path)
         self.currentSample = self.datasetDF.iloc[[0]]
         self.currentIdx = 0
@@ -178,13 +187,16 @@ class Impl_RiskWindow(Ui_RiskWindow, QtWidgets.QMainWindow):
             b.clicked.connect(self.btn_Page_clicked)
             
     def home_button_clicked(self):
+        from menu import Impl_MainWindow
+        self.p = Impl_MainWindow()
+        self.p.show()
         self.close()
         
     def go_back_button_clicked(self):
         self.close()
         
-    def closeEvent(self, event):
-        self.window_closed.emit("")
+   # def closeEvent(self, event):
+   #     self.window_closed.emit("")
 
     def cBox_GL_File_currentTextChanged(self):
         filepathCol = self.cBox_GL_File.currentText()
