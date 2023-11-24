b7c92ef4cd72f2315c5d56f947c598fdbf310113
aanthir1 <aanthir1@asu.edu>
2023-11-23 22:31:10 -0700

    fix toolbar of datasets.py

--- gui/menu.py
+++ gui/menu.py
@@ -42,13 +42,8 @@ class Impl_MainWindow(Ui_MainWindow, QtWidgets.QMainWindow):
         Loads and shows Datasets Window.
         """
         self.ds_ui = Impl_DatasetsWindow()
-        self.ds_ui.window_closed.connect(self.reloadScreen)
         self.ds_ui.show()
         self.close()
-        
-    def reloadScreen(self):
-        self.ds_ui = Impl_MainWindow()
-        self.ds_ui.show()
 
     def btn_Models_clicked(self):
         """Clicked event on btn_Models component.
